7e963a1515b9113b6f2fa0bdb4f56966dabc8295
kodaliasha <kodaliasha@gmail.com>
2015-05-29 12:58:41 -0700

    Angular js project using yoemen and other stuff.

---
+++ angular-app/app.js
@@ -0,0 +1,23 @@
+angular.module('angularAppModule', ['ui.bootstrap', 'ui.utils', 'ui.router', 'ngAnimate', 'dataServices', 'dataServices']);
+
+angular.module('angularAppModule').config(function($stateProvider, $urlRouterProvider) {
+
+    /* Add New States Above */
+    $urlRouterProvider.otherwise('/home');
+
+});
+
+angular.module('angularAppModule').run(function($rootScope) {
+
+    $rootScope.safeApply = function(fn) {
+        var phase = $rootScope.$$phase;
+        if (phase === '$apply' || phase === '$digest') {
+            if (fn && (typeof(fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$apply(fn);
+        }
+    };
+
+});
---
+++ angular-app/services/data-services.js
@@ -0,0 +1,10 @@
+angular.module('dataServices', ['ui.bootstrap','ui.utils','ui.router','ngAnimate']);
+
+angular.module('dataServices').config(function($stateProvider) {
+
+    /* Add New States Above */
+
+
+
+});
+
