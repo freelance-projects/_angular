6af65d61ad89689425b44d373cbe6507aff131d7
kodaliasha <kodaliasha@gmail.com>
2015-05-29 17:44:11 -0700

    product-store create

---
+++ angular-app/product-store/product-store.js
@@ -0,0 +1,8 @@
+angular.module('productStore', ['ui.bootstrap','ui.utils','ui.router','ngAnimate']);
+
+angular.module('productStore').config(function($stateProvider) {
+
+    /* Add New States Above */
+
+});
+
